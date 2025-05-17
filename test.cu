bc8396326b47c959af960b8f8ba759bd3335244d
Muhammad Salal Qadir <salalqadir05@gmail.com>
2025-05-17 08:10:06 -0700

    update test.cu to test

--- test.cu
+++ test.cu
@@ -9,5 +9,14 @@ int main() {
         return 1;
     }
     printf("Found %d CUDA devices\n", count);
+    
+    // Get device properties
+    for (int i = 0; i < count; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device %d: %s\n", i, prop.name);
+        printf("  Compute Capability: %d.%d\n", prop.major, prop.minor);
+        printf("  Total Memory: %lu MB\n", prop.totalGlobalMem / (1024*1024));
+    }
     return 0;
 } 
